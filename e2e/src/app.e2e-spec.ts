59f993dbadbcdccd0d6310e0927e6fbe07bddd1a
oudinia <oussama.dinia@gmail.com>
2021-01-21 15:35:57 +0000

    second run of ng new

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('angular-codeflow-pkce app is running!');
+    expect(await page.getTitleText()).toEqual('angular-oauth2-pkce app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-codeflow-pkce'),
+      dir: require('path').join(__dirname, './coverage/angular-oauth2-pkce'),
       subdir: '.',
       reporters: [
         { type: 'html' },
