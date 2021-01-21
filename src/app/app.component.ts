09cd0aa3fbc08d0793eb47bd13b1e14b4bc02c07
oudinia <oussama.dinia@gmail.com>
2021-01-21 16:09:55 +0000

    checkcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-oauth2-pkce';
+
+  constructor(public oidcSecurityService: OidcSecurityService) {}
+
+    ngOnInit() {
+      this.oidcSecurityService.authorize();
+        this.oidcSecurityService.checkAuth().subscribe((isAuthenticated) => {
+            console.log('app authenticated', isAuthenticated);
+            const at = this.oidcSecurityService.getToken();
+            console.log(`Current access token is '${at}'`);
+        });
+    }
 }
